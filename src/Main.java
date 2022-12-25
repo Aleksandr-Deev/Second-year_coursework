a84d3688e6bcf4bf690066099e9cd140271e1e09
Aleksandr Deev <geshtait@mail.ru>
2022-12-25 14:31:34 +0500

    Сделел курсовую 2 курса

--- src/Main.java
+++ src/Main.java
@@ -37,7 +37,7 @@ public class Main {
                             break label;
                     }
                 } else {
-//                    scanner.next();
+                    scanner.next();
                     System.out.println("Выберите пункт меню из списка!");
                 }
             }
